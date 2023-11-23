cce6be1dda18e0c3a8339ab79e649744a0d4420a
orangestranger <384663069@qq.com>
2023-11-23 14:35:24 +0800

    添加了常用计算函数

--- pythonLearing/1.int and float.py
+++ pythonLearing/1.int and float.py
@@ -13,3 +13,15 @@ print(type(2/2))  # 除法会输出浮点型
 print(type(3//2))  # 两个//符号：整除，可以使除出来的类型变为整型
 print(type(4%2))
 
+print(abs(1))  # abs函数
+print(abs(-1))  # 输出绝对值
+
+print(pow(3, 3))  # 求幂
+print((3**3))  # 表达式
+
+print(divmod(10,3))  # 求商和余数
+print(max(1, 2, 3, 4, 5, 6))  # 求最大值
+print(min(1, 2, 3, 4, 5, 6))  # 求最小值
+
+print(10/2.0)  # float
+print(10*20.0)  # float
