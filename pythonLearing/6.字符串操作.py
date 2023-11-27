80c1d6b60b4465c969f719edcb45f40dc9a5ad7b
orangestranger <384663069@qq.com>
2023-11-27 22:02:34 +0800

    添加了分割字符串操作

--- pythonLearing/6.字符串操作.py
+++ pythonLearing/6.字符串操作.py
@@ -7,9 +7,13 @@ print(bin(ord("A")))  # 输出对应二进制
 print(("abd exc".upper()))  # 转换成大写字母
 print("ABC DEG".lower())
 print("cnm".swapcase())
+print("abc def".split())
+print(" ".join(['abc', 'def']))  # 能将多个字符串链接成一个
 
 # capitalize()首字母大写
 # title()单词首字母大写
 # upper()字母全部大写
 # lower()字母全部小写
 # swapcase()大小写反转
+# split()将字符串分割成多个
+
