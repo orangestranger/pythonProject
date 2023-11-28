63f2544243c5a6bd7a2d12f729f1d64ffb416646
orangestranger <384663069@qq.com>
2023-11-28 23:41:37 +0800

    更新到第二小章节

---
+++ pythonLearing/2.组合数据类型/1.列表和列表方法.py
@@ -0,0 +1,15 @@
+a = "ppp-lll-qqq".split("-")  # 分割字符串，结果称之为列表
+print(a)
+print(type([a]))
+
+b = [1, 0.1, 4567+123j, "cyz"]  # 中括号中是列表
+print(type(b))
+
+# 增删列表元素的方法
+o = [1, 2, 3]
+print(o)
+o.append(4)  # 在末尾添加
+print(o)
+o.insert(0, "x")  # 添加第一位
+print(o)
+# 视频看至10：00
