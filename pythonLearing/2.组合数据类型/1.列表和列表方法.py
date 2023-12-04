92b7067ae04d65af4be4e916eee5e6ced3ab900c
orangestranger <384663069@qq.com>
2023-12-04 19:23:02 +0800

    update

--- pythonLearing/2.组合数据类型/1.列表和列表方法.py
+++ pythonLearing/2.组合数据类型/1.列表和列表方法.py
@@ -12,4 +12,20 @@ o.append(4)  # 在末尾添加
 print(o)
 o.insert(0, "x")  # 添加第一位
 print(o)
-# 视频看至10：00
+o.pop()  # 删除最后一位
+print(o)
+o.pop(0)  # 删除指定地方的值
+print(o)
+o.remove(2)  # remove方法需要写实际的值，找不到则报错，且只会删除第一个值，后续不会删除
+print(o)
+
+l = [1, 1, 2, 2, 3]
+print(l.index(1))  # 匹配到第一位的值，输出位置
+
+print(l.count(1))  # 计数1出现了几次
+
+l = [1, 4, 7, 1, 34, 6, 89, 22]
+l.sort()  # 排序
+print(l)
+l.reverse()  # 从大到小排序
+print(l)
