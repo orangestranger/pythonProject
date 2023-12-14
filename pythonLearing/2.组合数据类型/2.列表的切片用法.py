607d2ab849b592dcfd5b30985d27dbba6d99a8e4
orangestranger <384663069@qq.com>
2023-12-14 10:11:49 +0800

    update

--- pythonLearing/2.组合数据类型/2.列表的切片用法.py
+++ pythonLearing/2.组合数据类型/2.列表的切片用法.py
@@ -1,6 +1,21 @@
 l = [1, 2, 3, 4]
 print(l[0])
 print(l[-1])
+print(l[:])
+
+l[4:5] = ["x"]
+print(l[:])
+l[5:] = [6, 7, 8, 9]
+print(l[:])
+
+l = [1, 2, 3]
+l = l + [4, 'x', 6]
+print(l[:])
+
+print(l.index("x"))
+l = [1, 2, 3, 4]
+print(l[0])
+print(l[-1])
 
 l[4:5] = ["x"]
 print(l[:])
---
+++ pythonLearing/2.组合数据类型/4.字典.py
@@ -0,0 +1,1 @@
+# 字典（dict） 一种key-value映射结构，key不可重复
