6167e88fbc5ab88e12314e4834e74415b1a2dc2a
orangestranger <384663069@qq.com>
2023-12-19 19:31:01 +0800

    update

--- pythonLearing/3.程序控制结构/1.布尔值.py
+++ pythonLearing/3.程序控制结构/1.布尔值.py
@@ -2,3 +2,9 @@
 # 顺序结构
 # 分支结构（选择结构、条件结构）
 # 循环结构
+# 异常处理
+
+# boole布尔值，真，假
+print(type(False))
+print(type(bool(None)))
+# 所有非0的都是True，列表只要有内容布尔值就是真
---
+++ pythonLearing/3.程序控制结构/2.分支结构.py
@@ -0,0 +1,23 @@
+if True:
+    print('true')
+else:
+    print('false')
+
+if False:
+    print('true')
+else:
+    print('false')
+
+if 1:  # 隐式转换成布尔值
+    print('true')
+else:
+    print('false')
+
+if 0:  # 隐式转换成布尔值
+    print('true')
+else:
+    print('false')
+
+# 关系运算符号的优先级高于逻辑运算符
+# 关系运算符：!=, ==, is, is not, in, not in, >, >=, ==, <, <=
+# 逻辑运算符：and, or, not
