dc5d944e5268e96966aa3b5a0d7cffbb3a14ebba
orangestranger <384663069@qq.com>
2024-02-23 16:18:28 +0800

    update

--- pythonLearing/4.函数和代码复用/2.分支结构.py
+++ pythonLearing/4.函数和代码复用/2.分支结构.py
@@ -1,9 +1,10 @@
 a = input("a等于:")
-if a == 2:
+print(type(a))
+if a == '2':
     print("a = 2")
-elif a == 99:
+elif a == '99':
     print("a = 99")
-elif a != 2 or a != 99:
+elif a != '2' or a != '99':
     print("no 2 or 99")
 else:
     print("no idea")
