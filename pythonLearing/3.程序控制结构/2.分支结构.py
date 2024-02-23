7a810e09d624b69e5c3ed2b48bac9d60762ce1ba
orangestranger <384663069@qq.com>
2024-02-23 17:55:01 +0800

    update

--- pythonLearing/3.程序控制结构/2.分支结构.py
+++ pythonLearing/3.程序控制结构/2.分支结构.py
@@ -1,30 +1,10 @@
-if True:
-    print('true')
+a = input("a等于:")
+print(type(a))
+if a == '2':
+    print("a = 2")
+elif a == '99':
+    print("a = 99")
+elif a != '2' or a != '99':
+    print("no 2 or 99")
 else:
-    print('false')
-
-if False:
-    print('true')
-else:
-    print('false')
-
-if 1:  # 隐式转换成布尔值
-    print('true')
-else:
-    print('false')
-
-if 0:  # 隐式转换成布尔值
-    print('true')
-else:
-    print('false')
-
-# 关系运算符号的优先级高于逻辑运算符
-# 关系运算符：!=, ==, is, is not, in, not in, >, >=, ==, <, <=
-# 逻辑运算符：and, or, not
-
-print(True and False)
-print(True and True)  # 左右都为真才会输出真
-print(False and True)
-
-print(True or False)
-print(False or True)  # 有一边为真则为真
+    print("no idea")
