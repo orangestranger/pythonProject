35abff13676a5c03e055ec0612d1540587877226
orangestranger <384663069@qq.com>
2024-03-04 23:57:56 +0800

    update

--- pythonLearing/5.文件管理/2.0读写文件.py
+++ pythonLearing/5.文件管理/2.0读写文件.py
@@ -1,8 +1,16 @@
 def main():
+    # try:
+    #     filename = "text.txt"
+    #     with open(filename, "x") as f:
+    #         print("{}".format(filename))
+    # except Exception as e:
+    #     print("文件已经存在{}".format(e))
+    #     pass
     try:
         filename = "text.txt"
         with open(filename, "tr+", encoding="utf8") as f:
-            f.write(input(""))
+            # f.write(input("请直接输入:"))
+            print(f.read())
     except Exception as e:
         print("baocuo： {}".format(e))
 
@@ -10,3 +18,8 @@ main()
 
 
 
+# read([n])      按长度读取内容
+# readline()     按行数读取内容
+# readlines()    按行遍历内容
+# write(s)      写入指定内容
+# writeslines()  按行写入内容
